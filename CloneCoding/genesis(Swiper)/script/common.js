f4f6e8800c5748cb8448d8c3372fe715626dc568
Lim JunWoo <ceo7913@naver.com>
2023-11-13 16:48:39 +0900

    docs: [feat] gnbSwiper js add

--- CloneCoding/genesis(Swiper)/script/common.js
+++ CloneCoding/genesis(Swiper)/script/common.js
@@ -14,6 +14,12 @@ function gnbMenu(){
         // console.log(idx);
         el.addEventListener('click',()=> toggleMenu(subMenu[idx]))
     })
+
+    // subMenu 초기값 설정
+    subMenu.forEach((el)=>{
+        outMenuOver(el);
+    })
+
     function toggleMenu(index){
         if(isOpen == false){
             onMenuOver(index)
@@ -29,14 +35,49 @@ function gnbMenu(){
             menu.classList.remove('slide-up');
             menu.classList.add('slide-down');
             isOpen = true;
+
+            const rightItem = menu.querySelector('.subMenu-right');
+            if(rightItem){
+                rightItem.querySelector('.subMenu-right-model').classList.add('on');
+                rightItem.style.height = `${maxH}px`
+            }
+        })
+        // transition 을 최대한 안정적이게 실행 될 수 있게 하기 위해
+        // requestAnimationFrame 을 사용한다. / 없어도 실행되는대에는 문제가 없다.
+
+        // gnbswiper
+        const swiperContainer = menu.querySelectorAll('.subMenu-item .swiper-container');
+        swiperContainer.forEach((el)=>{
+            if(!el.classList.contains('swiper-initialized')){ 
+                console.log(el);
+                /*
+                    if (!container.classList.contains('찾을 class 명'))해당 클래스가 있는지 없는지 확인
+
+                    즉'(el = .swiper-container) 안에 
+                    className = swiper-initialized 가 없다면' 이라는 뜻
+                */
+                new Swiper(el,{
+                    slidesPerView : 'auto',
+                    navigation : {
+                        prevEl : '.swiper-button-prev',
+                        nextEl : '.swiper-button-next'
+                    },
+                    allowTouchMove : false, // 스와이프 기능 중지
+                    speed : 0, // 슬라이드 넘어가는 transition 속도 0
+                })
+            }
         })
     }
     function outMenuOver(menu){
-        requestAnimationFrame(()=>{
             menu.style.height = '0';
             menu.classList.remove('slide-down');
             menu.classList.add('slide-up');
             isOpen = false;
-        })
+
+            const rightItem = menu.querySelector('.subMenu-right');
+            if(rightItem){
+                rightItem.querySelector('.subMenu-right-model').classList.remove('on');
+                rightItem.style.height = 0;
+            }
     }
 }
