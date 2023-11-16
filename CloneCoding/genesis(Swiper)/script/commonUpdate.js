a3fd4774168a7a9ea81238501b54c0e0ca59314d
Lim JunWoo <ceo7913@naver.com>
2023-11-16 15:47:39 +0900

    feat: [CloneCoding] mainSlider auto play

--- CloneCoding/genesis(Swiper)/script/commonUpdate.js
+++ CloneCoding/genesis(Swiper)/script/commonUpdate.js
@@ -3,6 +3,7 @@ window.onload = () => {
   search();
   sitemap();
   visualSlider();
+  model();
 }
 
 function gnbMenu(){
@@ -114,7 +115,7 @@ function gnbMenu(){
   }
 
   
-}
+} // gnbMenu
 
 // 검색기능
 function search(){
@@ -144,7 +145,7 @@ function search(){
     searchWrapper.addEventListener('click',(e)=>{
         e.stopPropagation();
     })
-}
+} // search
 
 // 햄버거 버튼 기능
 /*
@@ -213,9 +214,13 @@ function sitemap(){
         el.style.height = maxH
         document.body.style.overflowY = overFlowAction;
     }
-}
+} // sitemap
 
 function visualSlider(){
+    const pausBtn = document.querySelector('.pause-btn');
+    let isPlay =true;
+
+    // swiper 기능정리
     const swiper = new Swiper('.visual-wrapper .swiper-container',{
         speed: 1000,
         loop: true,
@@ -237,4 +242,91 @@ function visualSlider(){
             clickable: true, // 클릭시 해당 페이지로 swipe
         }
     });
+    
+    // 메인 슬라이드 auto-play 버튼 제어 함수
+    pausBtn.addEventListener('click',function(){
+        if(isPlay){
+            swiper.autoplay.stop();
+            this.classList.add('on');
+            isPlay = false;
+        }else{
+            swiper.autoplay.start();
+            this.classList.remove('on');
+            isPlay = true;
+        }
+    });
+
+}// visualSlider
+
+function model(){
+    document.querySelectorAll('select').forEach((el)=>{
+        const optNum = el.children.length; // => 3
+        el.classList.add('select-hide') // 특정 클래스를 줘서 화면에서 제거
+        const wrapper = document.createElement('div'); // 객체를 생성
+        wrapper.className = 'select'; // className 지정
+        el.parentNode.insertBefore(wrapper, el);
+        wrapper.appendChild(el);
+
+        // li에서 선택한 텍스트를 출력시켜주는 태그 생성
+        const styleSelect = document.createElement('div');
+        styleSelect.className = 'select-style';
+        styleSelect.textContent = el.children[0].textContent;
+        el.after(styleSelect);
+
+        // list ul 생성
+        const list = document.createElement('ul');
+        list.className = 'select-list';
+        styleSelect.after(list); // styleSelect 뒤에 list 삽입 => 자식요소아님 말그대로 코드 구조상 뒤에/ 부모는 같을 수 있음
+        /*
+            생성된 코드
+            <div class="select">
+                <select class="select-hide">
+                    <option value="all" selected="">ALL</option>
+                    <option value="sedan">SEDAN</option>
+                    <option value="suv">SUV</option>
+                </select>
+                <div class="select-style">ALL</div>
+                <ul class="select-list"></ul></div>
+            </div>
+        */
+
+        // li 생성
+        for(let i = 0; i < optNum; i++){
+            let listItem = document.createElement('li');
+            listItem.textContent = el.children[i].textContent;
+            listItem.setAttribute('date-target', el.children[i].value);
+            list.appendChild(listItem);
+            /*
+                생성된 코드
+                <ul class="select-list">
+                    <li date-target="all">ALL</li>
+                    <li date-target="sedan">SEDAN</li>
+                    <li date-target="suv">SUV</li>
+                </ul>
+            */
+        }
+        const listItems = list.children;
+        console.log(listItems); // => HTMLCollection(3) [li, li, li]
+
+        // styleSelect 를 클릭했을때 list 창 생성하기
+        styleSelect.addEventListener('click',()=>{
+            document.querySelectorAll('.select-style.on').forEach((activeEl)=>{
+                if(activeEl !== styleSelect){
+                    activeEl.classList.remove('on');
+                    activeEl.nextSibling.style.display = 'none'
+                }
+            });
+            styleSelect.classList.toggle('on');
+            // ul 
+            list.style.display = list.style.display === 'block' ? 'none': 'block';
+        })
+        
+        // li 를 클릭하면 li 에 정보가 styleSelect에 전달하기
+        list.addEventListener('click',(e)=>{
+            const target = e.target;  // 클릭한 target 의 값 가져오기
+            if(target.tagName.toLowerCase() === 'li'){ // tagName => tagName 은 소문자가 아닌 대문자로 나옴 그렇기 때문에 toLowerCase() 를 사용해서 소문자로 변환해준다. 
+                
+            }
+        })
+    })
 }
