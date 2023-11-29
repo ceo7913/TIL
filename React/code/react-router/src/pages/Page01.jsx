99f8448bad9f6bdd9e1472243382c32b0e8b2c33
Lim JunWoo <ceo7913@naver.com>
2023-11-29 14:55:30 +0900

    docs: [React] react router dom

---
+++ React/code/react-router/src/pages/Page01.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Page01 = () => {
+  return (
+    <div>Page01</div>
+  )
+}
---
+++ React/code/react-router/src/pages/Page02.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Page02 = () => {
+  return (
+    <div>Page02</div>
+  )
+}
