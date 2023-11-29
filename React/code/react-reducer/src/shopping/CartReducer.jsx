11a271640ec64a5f82779628aa11870f464a4b30
Lim JunWoo <ceo7913@naver.com>
2023-11-29 17:42:59 +0900

    docs: [React] useReducer use example

--- React/code/react-reducer/src/shopping/CartReducer.jsx
+++ React/code/react-reducer/src/shopping/CartReducer.jsx
@@ -1,11 +1,52 @@
 // 상품카트 로직
 
 export const init = {
+    // 초기값
     items: [], // 몇개를 선택할지 모르기 때문에 배열로 담아준다. 빈배열 선언
     totalItems: 0, // 전체 선택 아이템의 갯수
     totalAmount: 0, // 전체 아이템의 가격
 }
 
-export function cartReducer(state,action){
-    
+// 사용 할 함수
+export function cartReducer(state, action){
+    let aa = {...state};
+    console.log(aa); // => {items: Array(2), totalItems: 2, totalAmount: 20000} => state 에 담긴 이전 값을 들고 있음
+    switch(action.type){
+        // 장바구니 버튼을 누르면, 선택한 item 의 name 이 빈 배열에 담기고 계속해서 클릭한 대상의 name 이 빈 배열에 담겨야 한다.
+        case 'add-item':
+            return {
+                // ... = 기존값
+                ...state,
+                items: [...state.items, action.pay],
+
+                // 클릭한 item 의 갯수를 나타냄
+                totalItems: state.totalItems+1,
+
+                // 클릭한 item 의 price 를 더해준다.
+                totalAmount: state.totalAmount + action.pay.price
+            }
+        case 'remove-item':
+            const itemIndex = state.items.findIndex(item => item.id === action.pay.id);
+            const updateItem = state.items.filter((_,index)=> index !== itemIndex);
+            const removeItem = state.items[itemIndex];
+            return{
+                ...state,
+                items: updateItem,
+                totalItems: state.totalItems -1,
+                totalAmount: state.totalAmount -removeItem.price
+            }
+        case 'update-item':
+            const updateItemIndex = state.items.findIndex(item => item.id === action.pay);
+            const itemUpdate = state.items[updateItemIndex];
+            const itemUpdateQu = {...state.itemUpdate, quantity : action.pay.quantity}
+            const itemUpdateList = state.items.map((el,index)=>
+            index === updateItemIndex ? updateItem: el)
+            console.log(updateItemIndex,itemUpdate,itemUpdateQu,itemUpdateList);
+            return{
+                ...state,
+                items : itemUpdateList,
+                totalAmount: state.items.reduce((total,item)=> total + item.price * item.quantity, 0)
+            }
+        default: break;
+    }
 }
--- React/code/react-reducer/src/shopping/ShoppingCart.jsx
+++ React/code/react-reducer/src/shopping/ShoppingCart.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { cartReducer, init } from './CartReducer';
 
 export const ShoppingCart = () => {
+    
     const items = [
         {id: 1, name: 'item1', price: 5000},
         {id: 2, name: 'item2', price: 15000},
@@ -8,6 +10,16 @@ export const ShoppingCart = () => {
         {id: 5, name: 'item5', price: 13000},
         {id: 6, name: 'item6', price: 11000},
     ]
+
+    const [state, dispatch] = useReducer(cartReducer, init);
+
+    const addItemCart =(product)=>{
+        dispatch({type: 'add-item', pay:product })
+    }
+    const removeItemCart =(product)=>{
+        dispatch({type: 'remove-item', pay:product })
+    }
+
   return (
     <div>
         <h1>아이템 리스트</h1>
@@ -16,10 +28,20 @@ export const ShoppingCart = () => {
                 <li key={el.id}>
                     <p>{el.name}</p>
                     <p>{el.price}</p>
-                    <button>장바구니</button>
+                    <button onClick={()=>addItemCart(el)}>장바구니</button>
+                    <button onClick={()=>removeItemCart(el)}>장바구니 삭제</button>
                 </li>
             ))}
         </ul>
+
+        <h1>카트 리스트</h1>
+        {state.items.map(el=>(
+            <div key={el.id}>
+                <p>{el.name} : {el.price}</p>
+            </div>
+        ))}
+        <p>Total Items : {state.totalItems}</p>
+        <p>Total Amount : {state.totalAmount}</p>
     </div>
   )
 }
